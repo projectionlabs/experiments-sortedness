b4f3bb9003eeaa7cdd39a4b660718d9ea3ee15bc
davips <dpsabc@gmail.com>
2022-08-03 17:40:40 -0300

    Release

--- src/experimentssortedness/config.py
+++ src/experimentssortedness/config.py
@@ -1,8 +1,12 @@
 from configparser import ConfigParser
 from pathlib import Path
 
-
 config = ConfigParser()
 config.read(f"{Path.home()}/.sortedness-cache.config")
-local_cache_uri = config.get("storages", "local")
-remote_cache_uri = config.get("storages", "remote")
+try:  # pragma: no cover
+    local_cache_uri = config.get("storages", "local")
+    remote_cache_uri = config.get("storages", "remote")
+except Exception as e:
+    print(
+        "Please create a config file '.sortedness-cache.config' in your home folder following the template file at the github repository."
+    )
